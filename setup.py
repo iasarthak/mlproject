d500f96f82a1fc22dd582df26ab8cbc12065bb6a
Sarthak Arora <sarthak@Sarthaks-MacBook-Air.local>
2023-08-17 20:05:50 +0530

    modified setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ def get_requirements(file_path: str) -> List[str]:
     """
     this function will return the list of requirements from the requirements.txt file
     """
-    requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         [req.replace("\n", "") for req in requirements]
