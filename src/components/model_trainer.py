d725deefc91a834a76fd1523b6de8d0a3f875bd9
Sarthak Arora <sarthak@Sarthaks-MacBook-Air.local>
2023-09-10 16:35:33 +0530

    Hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,8 +46,40 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                },
+                "Linear Regression": {},
+                "K-Neighbors Regressor": {
+                    'n_neighbors': [5,7,9,11],
+                },
+                "XGB Regressor": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                    'learning_rate': [.1, .01, .05, .001],
+                },
+                 "CatBoosting Classifier": {
+                     'depth': [6, 8, 10],
+                     'learning_rate': [0.01, 0.05, 0.1],
+                     'iterations': [30, 50, 100],
+                 },
+                "AdaBoost Regressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                }
+
+            }
+
             model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
-                                                 models=models)
+                                                 models=models, params=params)
 
             # to get best model score from dict
             best_model_score = max(model_report.values())
@@ -68,5 +100,5 @@ class ModelTrainer:
             r_squared = r2_score(y_test, predicted)
             return r_squared
 
-        except:
-            pass
+        except Exception as e:
+            raise CustomException(e, sys)
